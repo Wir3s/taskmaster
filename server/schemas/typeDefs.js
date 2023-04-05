890e8e67e6eeb1bedbfdf06ff9c977386ea9cd1a
Wir3s <aandrebess@gmail.com>
2023-04-05 18:57:28 -0500

    Building User and Task

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,15 +3,27 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 
 type User {
-
+    _id: ID
+    email: String!
+    username: String!
+    password: String!
 }
 
 type Task {
-
+    _id: ID
+    taskTitle: String!
+    taskDesc: String
+    taskPriority: Int
+    taskComplete: Boolean!
+    taskAssignees: [User]
+    subTasks: [Task]
+    createdBy: [User]
+    createdAt: Date
+    dueDate: Date
 }
 
 type Tasklist {
-    
+
 }
 
 
