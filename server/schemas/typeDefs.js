35c5faf7923f7802345daaa4fb5d53c35c84bb58
Wir3s <aandrebess@gmail.com>
2023-04-05 18:40:06 -0500

    skeleton of typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,31 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type User {
+
+}
+
+type Task {
+
+}
+
+type Tasklist {
+    
+}
+
+
+
+type Query {
+
+}
+
+
+type Mutation {
+
+
+}
+
+`;
+
+module.exports = typeDefs;
