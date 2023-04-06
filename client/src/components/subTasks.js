56ad2cca713f728144269f7ee66314711229b2f1
Garrett Winter <Garrett_Winter@hotmail.com>
2023-04-06 12:11:27 -0500

    Initial commit for the subtasks component.

---
+++ client/src/components/subTasks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+
+const subTasks = () => {
+    return (
+      <Container maxWidth="full">
+        SubTasks Diplay
+      </Container>
+    );
+  };
+  
+  export default subTasks;
