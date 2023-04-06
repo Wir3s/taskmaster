eabf84811a5ba0f83d19f6c17e8d4d5cb5d2340e
Garrett Winter <Garrett_Winter@hotmail.com>
2023-04-06 12:07:02 -0500

    Initial Commit for the taskList component

---
+++ client/src/components/taskList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+
+const TaskList = () => {
+    return (
+      <Container maxWidth="full" disableGutters={true}>
+        Task List
+      </Container>
+    );
+  };
+  
+  export default TaskList;
