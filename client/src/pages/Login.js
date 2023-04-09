8bfbba38a005617ba41cd2a56961b2d25bd91e0b
karlnh <hubigkarl@gmail.com>
2023-04-09 14:29:57 -0500

    uncomment login- and token-related stuff

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-// import { useMutation } from '@apollo/client';
-// import { LOGIN } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 // Commented portions relate to code that isn't developed yet.
 // Might need to change the import name of the LOGIN mutation later.
 
 const Login = (props) => {
     const [formState, setFormState] = useState({ email: '', password: ''});
-    // const [login, { error, data }] = useMutation(LOGIN);
+    const [login, { error, data }] = useMutation(LOGIN);
 
     // update form state
     const handleChange = (event) => {
@@ -24,11 +25,11 @@ const Login = (props) => {
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         try {
-            const { data } = await Login({
+            const { data } = await login({
                 variables: { ...formState },
             });
 
-            // Auth.login(data.login.token);
+            Auth.login(data.login.token);
         } catch (error) {
             console.error(error);
         }
@@ -43,28 +44,34 @@ const Login = (props) => {
     return (
         <main>
             <div>
-                <h1>Login</h1>
-                <form onSubmit={handleFormSubmit}>
-                    <input
-                        placeholder="Enter email..."
-                        name="email"
-                        type="email"
-                        value={formState.email}
-                        onChange={handleChange}
-                    />
-                    <input
-                        placeholder="Enter password..."
-                        name="password"
-                        type="password"
-                        value={formState.password}
-                        onChange={handleChange}
-                    />
-                    <button
-                        type="submit"
-                    >
-                        Submit
-                    </button>  
-                </form> 
+                { data
+                ? (<p>There is data</p>)
+                : (
+                <div>
+                    <h1>Login</h1>
+                    <form onSubmit={handleFormSubmit}>
+                        <input
+                            placeholder="Enter email..."
+                            name="email"
+                            type="email"
+                            value={formState.email}
+                            onChange={handleChange}
+                        />
+                        <input
+                            placeholder="Enter password..."
+                            name="password"
+                            type="password"
+                            value={formState.password}
+                            onChange={handleChange}
+                        />
+                        <button
+                            type="submit"
+                        >
+                            Submit
+                        </button>  
+                    </form>
+                </div>
+                )}
             </div>
             <div>
                 <Link to="/signup">
