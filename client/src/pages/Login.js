94aa4f799e8177bc8246b7ed89e8ba79e094bdce
karlnh <hubigkarl@gmail.com>
2023-04-08 23:30:16 -0500

    import mutations, add basic login function

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+// import { LOGIN } from '../utils/mutations';
 
-const Login = () => {
-    return (
+const Login = (props) => {
+    const [formState, setFormState] = useState({ email: '', password: ''});
+    // const [login, { error, data }] = useMutation(LOGIN);
+
+     return (
         <div>
             <h1>Login</h1>
         </div>
