369918b3b5f1d4ad40d11fa4cf55d2a5119e2a10
karlnh <hubigkarl@gmail.com>
2023-04-10 22:00:40 -0500

    redirect to login from settings

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+import Auth from "../utils/auth";
 
 const Settings = () => {
+    if (!Auth.loggedIn()) {
+        
+        return (
+            <div>
+                {console.log("Not logged in. Redirecting...")}
+                <Navigate to='/login' />
+            </div>
+
+
+        )
+    }
     return (
         <div>
             <h1>Settings</h1>
