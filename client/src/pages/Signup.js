275cc62ac180b4bd3ef771ac4a33e762a403a63f
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-04-12 06:46:15 -0500

    Working on implementing signup, throwing error regarding _id but account created in compass

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,87 +1,99 @@
 import React, { useState } from "react";
-// import { useMutation } from '@apollo/client';
-import { Link, Navigate } from 'react-router-dom';
+import { useMutation } from "@apollo/client";
+import { Link, Navigate } from "react-router-dom";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
-import Auth from '../utils/auth';
+const Signup = (props) => {
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-const Signup = () => {
-    const [formState, setFormState] = useState({
-        username: '',
-        email: '',
-        password: '',
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
     });
-    // const [addUser, {error, data }] = useMutation(ADD_USER);
+  };
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(data);
+    console.log({ data });
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
 
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
-
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        
-        try {
-            // const { data } = await addUser({
-            //     variables: { ...formState },
-            // });
-
-            // Auth.login(data.addUser.token);
-        } catch (err) {
-            console.log(err)
-        }
+      Auth.login(data.login.token);
+    } catch (error) {
+      console.error(error);
     }
 
-    if (Auth.loggedIn()) {
-        return <Navigate to='/home' />
-    }
+    setFormState({
+      username: "",
+      email: "",
+      password: "",
+    });
+  };
 
+  if (Auth.loggedIn()) {
     return (
-        <main>
-            <div>
-                <h1>Signup</h1>
-                <form onSubmit={handleFormSubmit}>
-                    <input
-                        placeholder="Enter username..."
-                        name="username"
-                        type="text"
-                        value={formState.username}
-                        onChange={handleChange}
-                    />
-                    <input
-                        placeholder="Enter email..."
-                        name="email"
-                        type="email"
-                        value={formState.email}
-                        onChange={handleChange}
-                    />
-                    <input
-                        placeholder="Enter password..."
-                        name="password"
-                        type="password"
-                        value={formState.password}
-                        onChange={handleChange}
-                    />
-                    <button
-                        type="submit"
-                    >
-                        Signup
-                    </button>
-                </form>
-            </div>
-            <div>
-                <Link to="/login">
-                    <button>
-                        Back to login.
-                    </button>
-                </Link>
-            </div>
-        </main>
+      <div>
+        {console.log("Already logged in. Redirecting...")}
+        <Navigate to="/home" />
+      </div>
+    );
+  }
 
-    )
+  return (
+    <main>
+      <div>
+        {data ? (
+          <p>Signing up...</p>
+        ) : (
+          <div>
+            <h1>Signup</h1>
+            <form onSubmit={handleFormSubmit}>
+              <input
+                placeholder="Enter username..."
+                name="username"
+                type="text"
+                value={formState.username}
+                onChange={handleChange}
+              />
+              <input
+                placeholder="Enter email..."
+                name="email"
+                type="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                placeholder="Enter password..."
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button type="submit">Signup</button>
+            </form>
+          </div>
+        )}
+      </div>
+      <div>
+        <Link to="/login">
+          <button>Back to login.</button>
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default Signup;
