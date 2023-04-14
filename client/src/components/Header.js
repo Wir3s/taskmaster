c3225d35b00f1968537d162fcc60ad906604a9f6
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-04-14 06:18:26 -0500

    Logout button added to header, span class needs to align right

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Toolbar, Button } from "@mui/material";
+import Auth from '../utils/auth'
 
 export default function Header() {
+    const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
   const displayDesktop = () => {
-    return <Toolbar>TASKMASTER</Toolbar>;
+    return <Toolbar>TASKMASTER      
+      <span>{Auth.loggedIn() ? ( 
+        <Button onClick={logout} color="error">Logout</Button>
+      ) : (
+        <h5>Login</h5>
+        
+      )}</span>
+      </Toolbar>;
   };
 
   return (
     <header>
       <AppBar>{displayDesktop()}</AppBar>
+
     </header>
   );
 }
