58f9e20e0a1b78c1122d8f1a2b8be8d777f6030f
Wir3s <aandrebess@gmail.com>
2023-04-13 19:14:05 -0500

    Styling branch created, uncommented two items in the page wrapper

--- client/src/components/pageWrapper.js
+++ client/src/components/pageWrapper.js
@@ -1,16 +1,16 @@
-export default function PageWrapper(props){
-    console.log(props)
-    return (
-        <div style=
-        {{   
-            display: 'flex',
-            flexFlow: 'row',
-            backgroundColor: 'grey',
-            margin: '5vh',
-            padding: '2vh',
-        }}
-        >
-        {props.children}
-        </div>
-    )
-}; 
+export default function PageWrapper(props) {
+  console.log(props);
+  return (
+    <div
+      style={{
+        // display: 'flex',
+        // flexFlow: 'row',
+        backgroundColor: "grey",
+        margin: "5vh",
+        padding: "2vh",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+}
