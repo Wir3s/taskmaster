be0db02163c803bc17d6ad7a6ed555ff56f28cc4
karlnh <hubigkarl@gmail.com>
2023-04-14 21:21:11 -0500

    change the filter!

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { BottomNavigation } from '@mui/material';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+
 const Footer = () => {
     return (
-        <footer>
-            <h3>Lovely footer designed by all of us here at TaskMaster ♥</h3>
-        </footer>
+        // <footer style={{
+        //     position: 'sticky',
+        //     textAlign: 'center'
+        // }}>
+        //     <h3>Lovely footer designed by all of us here at TaskMaster ♥</h3>
+        // </footer>
+        <BottomNavigation style={{backgroundColor: "transparent"}}>
+            <p style={{display: 'flex', flexFlow: 'row wrap', alignItems: 'center'}}>
+                <AutoAwesomeIcon/><span style={{margin: "3vh"}}>Change The Filter 2023</span><AutoAwesomeIcon/>
+            </p>
+        </BottomNavigation>
     )
 };
 
