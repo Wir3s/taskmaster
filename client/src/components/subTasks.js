8b1761caa76cd6716198bff74384906061a93f8e
Wir3s <aandrebess@gmail.com>
2023-04-14 21:42:37 -0500

    Fixes error Each child in a list should have a unique 'key' prop

--- client/src/components/subTasks.js
+++ client/src/components/subTasks.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import {
   Box,
   Collapse,
@@ -11,23 +11,23 @@ import {
   TableRow,
   Typography,
   Paper,
-  Container
-} from '@mui/material';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+  Container,
+} from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
-import DeleteTaskDialog from './deleteTaskDialog'
-import DeleteSubTaskDialog from './deleteSubTaskDialog';
+import DeleteTaskDialog from "./deleteTaskDialog";
+import DeleteSubTaskDialog from "./deleteSubTaskDialog";
 
-import { useQuery } from '@apollo/client';
-import { GET_SINGLE_LIST } from '../utils/queries';
+import { useQuery } from "@apollo/client";
+import { GET_SINGLE_LIST } from "../utils/queries";
 
-import NewTaskModal from './newTaskModal';
-import UpdateTaskModal from './updateTaskModal';
-import NewSubTaskModal from './newSubTaskModal';
-import UpdateSubTaskModal from './updateSubTaskModal';
+import NewTaskModal from "./newTaskModal";
+import UpdateTaskModal from "./updateTaskModal";
+import NewSubTaskModal from "./newSubTaskModal";
+import UpdateSubTaskModal from "./updateSubTaskModal";
 
-import ListContext from './listContext';
+import ListContext from "./listContext";
 
 // const headers = [
 //   {
@@ -58,16 +58,16 @@ import ListContext from './listContext';
 
 const styles = {
   header: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   main: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center'
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
   },
-}
+};
 
 export default function SubTasks() {
   const [open, setOpen] = React.useState(false);
@@ -75,35 +75,40 @@ export default function SubTasks() {
   const { activeList, setData } = useContext(ListContext);
 
   const id = activeList; // This is the list ID
-  const { loading, error, data } = useQuery(GET_SINGLE_LIST,
-    { variables: { id } }
-  )
+  const { loading, error, data } = useQuery(GET_SINGLE_LIST, {
+    variables: { id },
+  });
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>No List has been select, please choose one from above.</p>;
+  if (error)
+    return <p>No List has been select, please choose one from above.</p>;
 
   const listData = data?.list;
 
-  console.log(listData)
+  console.log(listData);
 
   return (
     <Container>
-      <Container >
+      <Container>
         <div id="subTaskContainerHeader" style={styles.header}>
-          <div style={{
-            display: 'flex',
-            flexFlow: 'row wrap',
-            alignItems: "center"
-          }}>
+          <div
+            style={{
+              display: "flex",
+              flexFlow: "row wrap",
+              alignItems: "center",
+            }}
+          >
             <h2>{listData.listName}</h2>
-            <p style={{ margin: '1vh' }}>Task Count</p>
+            <p style={{ margin: "1vh" }}>Task Count</p>
           </div>
-          <div style={{
-            display: 'flex',
-            flexFlow: 'row-reverse wrap'
-          }}>
+          <div
+            style={{
+              display: "flex",
+              flexFlow: "row-reverse wrap",
+            }}
+          >
             <NewTaskModal />
-            <p style={{ margin: '1vh' }}>Toggle Completed Tasks</p>
+            <p style={{ margin: "1vh" }}>Toggle Completed Tasks</p>
           </div>
         </div>
       </Container>
@@ -111,28 +116,32 @@ export default function SubTasks() {
         <Table aria-label="collapsible table">
           <TableHead>
             <TableRow>
-            {/* {headers.map((header) => (
+              {/* {headers.map((header) => (
               <TableCell key={header.headerId} align="left">{header.headerText}</TableCell>
             ))} */}
-            <TableCell align="left">Hide / Show Subtasks</TableCell>
+              <TableCell align="left">Hide / Show Subtasks</TableCell>
               <TableCell align="left">Details</TableCell>
               <TableCell align="left">Priority</TableCell>
               <TableCell align="left">Task Name</TableCell>
               <TableCell align="left">Due Date</TableCell>
               <TableCell align="left">Actions</TableCell>
-              </TableRow>
+            </TableRow>
           </TableHead>
           <TableBody>
             {listData.tasks.map((row) => (
-              <React.Fragment>
-                <TableRow key={row._id} sx={{ '& > *': { borderBottom: 'unset' } }}>
+              <React.Fragment key={row._id}>
+                <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
                   <TableCell align="left">
                     <IconButton
                       aria-label="expand row"
                       size="small"
                       onClick={() => setOpen(!open)}
                     >
-                      {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                      {open ? (
+                        <KeyboardArrowUpIcon />
+                      ) : (
+                        <KeyboardArrowDownIcon />
+                      )}
                     </IconButton>
                   </TableCell>
                   <TableCell align="left">
@@ -141,19 +150,27 @@ export default function SubTasks() {
                       taskName={row.title}
                       taskPriority={row.priority}
                       taskDueDate={row.dueDate}
-                      taskDesc={row.desc} /></TableCell>
+                      taskDesc={row.desc}
+                    />
+                  </TableCell>
                   <TableCell align="left">{row.priority}</TableCell>
                   <TableCell align="left">{row.title}</TableCell>
                   <TableCell align="left">{row.dueDate}</TableCell>
-                  <TableCell align="left"><DeleteTaskDialog
-                    taskId={row._id}
-                    taskName={row.title}
-                    taskPriority={row.priority}
-                    taskDueDate={row.dueDate}
-                    taskDesc={row.desc} /></TableCell>
+                  <TableCell align="left">
+                    <DeleteTaskDialog
+                      taskId={row._id}
+                      taskName={row.title}
+                      taskPriority={row.priority}
+                      taskDueDate={row.dueDate}
+                      taskDesc={row.desc}
+                    />
+                  </TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                  <TableCell
+                    style={{ paddingBottom: 0, paddingTop: 0 }}
+                    colSpan={6}
+                  >
                     <Collapse in={open} timeout="auto" unmountOnExit>
                       <Box sx={{ margin: 1 }}>
                         <Typography variant="h6" gutterBottom component="div">
@@ -174,15 +191,21 @@ export default function SubTasks() {
                                     subTaskDesc={subTask.desc}
                                   />
                                 </TableCell>
-                                <TableCell align="left">{subTask.priority}</TableCell>
-                                <TableCell align="left">{subTask.title}</TableCell>
+                                <TableCell align="left">
+                                  {subTask.priority}
+                                </TableCell>
+                                <TableCell align="left">
+                                  {subTask.title}
+                                </TableCell>
                                 <TableCell align="left">
                                   <DeleteSubTaskDialog
                                     subTaskId={subTask._id}
                                     taskId={row._id}
                                     subTaskName={subTask.title}
                                     subTaskPriority={subTask.priority}
-                                    subTaskDesc={subTask.desc} /></TableCell>
+                                    subTaskDesc={subTask.desc}
+                                  />
+                                </TableCell>
                               </TableRow>
                             ))}
                           </TableBody>
