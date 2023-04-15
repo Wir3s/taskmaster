a41ec3fdb6cb0bfee207398cf4d08cd06969063a
Wir3s <aandrebess@gmail.com>
2023-04-15 14:50:32 -0500

    Added a logo to the header with styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppBar, Toolbar, Button, Grid } from "@mui/material";
-import Auth from '../utils/auth'
+import Auth from "../utils/auth";
 
 export default function Header() {
   const logout = (event) => {
@@ -9,27 +9,39 @@ export default function Header() {
   };
 
   const displayDesktop = () => {
-    return <Toolbar>
-      <Grid container
-      direction="row"
-      justifyContent="space-between"
-      alignItems="center"
-      component="span"
-      >
-        <Grid item>
-          <h1>TASKMASTER</h1>
+    return (
+      <Toolbar>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          component="span"
+        >
+          <Grid item>
+            <img
+              src="assets/images/tmlogo.png"
+              alt="TaskMaster Logo"
+              width="100"
+              className="logo"
+            />
+          </Grid>
+          <Grid item>
+            <h1>TASKMASTER</h1>
+          </Grid>
+          <Grid item>
+            {
+              // render logout button if logged in
+              Auth.loggedIn() && (
+                <Button id="headerLogoutBtn" color="inherit" onClick={logout}>
+                  Logout
+                </Button>
+              )
+            }
+          </Grid>
         </Grid>
-        <Grid item>
-          {
-          // render logout button if logged in
-          Auth.loggedIn()
-          &&  <Button id="headerLogoutBtn" color="inherit" onClick={logout}>
-                Logout
-              </Button>
-          }
-        </Grid>
-      </Grid>
-      </Toolbar>;
+      </Toolbar>
+    );
   };
 
   return (
