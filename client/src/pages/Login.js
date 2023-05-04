7bd6918b3abb242dc4b60087b5a205d78aff7201
Wir3s <113479857+Wir3s@users.noreply.github.com>
2023-05-04 06:34:55 -0500

    Changed login error message to modal

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Modal, Box, Typography } from "@mui/material";
 import { useMutation } from "@apollo/client";
 import { LOGIN } from "../utils/mutations";
 import Auth from "../utils/auth";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 const Login = () => {
   const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { data }] = useMutation(LOGIN);
+
+  const [open, setOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   // update form state
   const handleChange = (event) => {
@@ -30,12 +45,13 @@ const Login = () => {
     } catch (error) {
       console.error(error);
       if (error.message.includes("No user with this email")) {
-        alert("This email address is not registered. Please sign up!");
+        setErrorMessage("The email address you entered is not registered.");
       } else if (error.message.includes("Incorrect password")) {
-        alert("The password is incorrect");
+        setErrorMessage("The password is incorrect");
       } else {
-        alert("An error occurred while logging in. Please try again.");
+        setErrorMessage("An error occurred while logging in. Please try again.");
       }
+      setOpen(true);
     }
 
     // reset form state after submit
@@ -43,6 +59,10 @@ const Login = () => {
       email: "",
       password: "",
     });
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   if (Auth.loggedIn()) {
@@ -117,9 +137,33 @@ const Login = () => {
               </Link>
             </form>
           </div>
+
         )}
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="ErrorModal" variant="h6" component="h2" sx={{ mb: 2 }}>
+            Something went wrong -
+          </Typography>
+          <Typography id="ErrorModalMessage" sx={{ mb: 2 }}>
+            {errorMessage}
+            {errorMessage.includes("is not registered") && (
+            <Button component="a" href="/signup" variant="outlined" color="secondary" size="small">
+              Sign Up
+            </Button>
+          )}
+          </Typography>
+          <Button onClick={handleClose}>Close</Button>
+        </Box>
+      </Modal>
     </main>
+
+    
   );
 };
 
